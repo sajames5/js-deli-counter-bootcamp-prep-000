37fc95fffcd068bdde295750f176e9b6a851ffd1
Stephanie <s.a.james5@gmail.com>
2018-02-13 06:18:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeli = []
+
+function takeANumber(katzDeli, name) {
+  for (let i = 3; i < 4; i++) {
+    array.push(`Welcome name, you are number 1 in line`)}
+    return 
+}
+var otherDeli = ["Steven", "Blake", "Avi"]
+
+function takeANumber(otherDeli, name) {
+  var newDeli = [...number, name]
+  return newDeli}
+ 
+  
+  
